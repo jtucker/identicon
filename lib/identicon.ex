ad263c3bcd2a19ecab833126104fda0ce7ccb720
Jason Tucker <jtucker@gmail.com>
2017-09-17 09:30:16 -0400

    Generating the color of identicon Building the grid for the identicon

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,10 +2,29 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row(row) do
+    [first, second | _] = row
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [red, green, blue | _]} = image) do
+    %Identicon.Image{image | color: {red, green, blue}}
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
